947f44ccc5b0c5ababec280c4bcd37eb703b1fb1
Code558 <savvyshreyas223@gmail.com>
2025-05-08 11:41:58 -0600

    started

---
+++ src/java/controller/Controller.java
@@ -0,0 +1,14 @@
+package java.controller;
+
+import javax.swing.JOptionPane;
+
+public class Controller
+{
+	
+	
+	public void start()
+	{
+		JOptionPane.showMessageDialog(window, "Welcome to Java art!");
+		
+	}
+}
